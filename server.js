d32b5570634395f3b915aaf7e067abcb32578bca
tanajeannai3 <tanajeannai3@gmail.com>
2020-02-22 13:11:06 -0600

    adding to package.json

--- server.js
+++ server.js
@@ -27,6 +27,15 @@ app.use(bodyParser.json());
 
 app.use('/api', routes);
 
+if (process.env.NODE_ENV === 'production') {
+    // Serve any static files
+    app.use(express.static(path.join(__dirname, 'client/build')));
+  // Handle React routing, return all requests to React app
+    app.get('*', function(req, res) {
+      res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+    });
+  }
+
 app.use((err, req, res, next) => {
   console.log(err);
   next();
